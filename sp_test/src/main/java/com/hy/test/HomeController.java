7f8edb93ada17e564d8e08c1e54608a07628d6ab
User <User@UserPC>
2017-04-07 14:51:55 +0900

    asd

--- sp_test/src/main/java/com/hy/test/HomeController.java
+++ sp_test/src/main/java/com/hy/test/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//dasdasdas
 @Controller
 public class HomeController {
 	
